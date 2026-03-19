ba3e0aff11983dacdd030d4846f1ac8ba2f142ef
James Humphries <james@james-humphries.co.uk>
2026-03-19 10:18:25 +0000

    feedback addressing
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- internal/states/instance_object_src.go
+++ internal/states/instance_object_src.go
@@ -74,10 +74,10 @@ type ResourceInstanceObjectSrc struct {
 	CreateBeforeDestroy bool
 	SkipDestroy         bool
 
-	// TODO: godoc
-	IdentityJSON []byte
-
-	// TODO: godoc
+	// IdentityJSON contains a JSON-encoded representation of the resourcde identity for this
+	// resource instance. Similar to AttrsJSON, this is handled in JSON format because
+	// schema versions change over time.
+	IdentityJSON          []byte
 	IdentitySchemaVersion *uint64
 }
 
